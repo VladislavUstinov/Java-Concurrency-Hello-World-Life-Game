6dc6564c8a309c4c0226ea0e58b2faa0f06c03e1
Vladislav Ustinov <vladustinov90@gmail.com>
2021-02-17 17:02:49 +0300

    123

---
+++ src/testfieldgame/TestFieldGame.java
@@ -0,0 +1,16 @@
+
+package testfieldgame;
+
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class TestFieldGame {
+    /**
+     * See the {@link GameTest} JUnit test. It contains main launch of the {@link Game#start} function.
+     */
+    public static void main(String[] args) {        
+    }
+}
+
